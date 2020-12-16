6c892079f8dfb3392827fda4b78f11ebc5907829
Magnus Ulf <magnus@muj.dk>
2020-12-16 18:51:04 +0100

    Compatibility for 1.16

--- src/com/massivecraft/massivecore/command/type/enumeration/TypeEntityType.java
+++ src/com/massivecraft/massivecore/command/type/enumeration/TypeEntityType.java
@@ -1,9 +1,6 @@
 package com.massivecraft.massivecore.command.type.enumeration;
 
-import com.massivecraft.massivecore.collections.MassiveSet;
 import org.bukkit.entity.EntityType;
-
-import java.util.Set;
 
 public class TypeEntityType extends TypeEnum<EntityType>
 {
@@ -16,26 +13,6 @@ public class TypeEntityType extends TypeEnum<EntityType>
 	public TypeEntityType()
 	{
 		super(EntityType.class);
-	}
-	
-	// -------------------------------------------- //
-	// OVERRIDE
-	// -------------------------------------------- //
-	
-	@Override
-	public Set<String> getNamesInner(EntityType value)
-	{
-		Set<String> ret = new MassiveSet<>(super.getNamesInner(value));
-		
-		if (value == EntityType.PIG_ZOMBIE)
-		{
-			ret.add("pigman");
-			ret.add("pigzombie");
-			ret.add("manpig");
-			ret.add("zombiepig");
-		}
-		
-		return ret;
 	}
 
 }
